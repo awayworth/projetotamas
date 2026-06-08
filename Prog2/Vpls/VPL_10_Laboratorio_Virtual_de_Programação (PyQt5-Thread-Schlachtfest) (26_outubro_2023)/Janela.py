6edb32a8f4362fc46b39e68d788251abfef80d53
spyestrela <ester130.10@gmail.com>
2026-06-08 19:33:44 -0300

    Melhorias feitas
    O vpl está funcional e nota 10

--- Prog2/Vpls/VPL_10_Laboratorio_Virtual_de_Programação (PyQt5-Thread-Schlachtfest) (26_outubro_2023)/Janela.py
+++ Prog2/Vpls/VPL_10_Laboratorio_Virtual_de_Programação (PyQt5-Thread-Schlachtfest) (26_outubro_2023)/Janela.py
@@ -1,72 +1,71 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QLineEdit, QPushButton, QGridLayout
-from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 from ThreadSchlacht import ThreadSchlacht
 
 class Janela(QWidget):
-    _LEd1 = None
-    _LEd2 = None
-    _LEd3 = None
-    _Bt_Abertura = None
-    _Bt_Fechamento = None
-    Barraca1 = None
-    Barraca2 = None
-    Barraca3 = None
+    __LEd1=None
+    __LEd2=None
+    __LEd3=None
+    __Bt_Abertura=None
+    __Bt_Fechamento=None
+    __Barraca1=None
+    __Barraca2=None
+    __Barraca3=None
 
     def __init__(self, Str="Janela", x1=400, y1=200, dx=640, dy=480, cor="orange"):
         super().__init__()
         self.setWindowTitle(Str)
         self.setGeometry(x1, y1, dx, dy)
         self.setAutoFillBackground(True)
-        
         p = self.palette()
         p.setColor(self.backgroundRole(), QColor(cor))
         self.setPalette(p)
-        
         self.inicialize()
 
     def closeEvent(self, event):
-        self.Barraca1.parar()
-        self.Barraca2.parar()
-        self.Barraca3.parar()
+        self.__Barraca1.parar()
+        self.__Barraca2.parar()
+        self.__Barraca3.parar()
         self.destroy()
         sys.exit(0)
 
     def action_abertura(self):
-        self.Barraca1.iniciar(60)
-        self.Barraca2.iniciar(60)
-        self.Barraca3.iniciar(60)
+        self.__Barraca1.iniciar(60)
+        self.__Barraca2.iniciar(60)
+        self.__Barraca3.iniciar(60)
 
     def action_fechamento(self):
-        self.Barraca1.parar()
-        self.Barraca2.parar()
-        self.Barraca3.parar()
+        self.__Barraca1.parar()
+        self.__Barraca2.parar()
+        self.__Barraca3.parar()
 
     def inicialize(self):
-        grid = QGridLayout()
-        self.setLayout(grid)
+        Grid = QGridLayout()
+    
+        self.__LEd1 = QLineEdit()
+        self.__LEd2 = QLineEdit()
+        self.__LEd3 = QLineEdit()
+        self.__LEd1.setText('0')
+        self.__LEd2.setText('0')
+        self.__LEd3.setText('0')
+    
+        self.__Bt_Abertura   = QPushButton('Abertura')
+        self.__Bt_Fechamento = QPushButton('Fechamento')
+    
+        self.__Bt_Abertura.clicked.connect(self.action_abertura)
+        self.__Bt_Fechamento.clicked.connect(self.action_fechamento)
+    
+        self.__Barraca1 = ThreadSchlacht(self.__LEd1)
+        self.__Barraca2 = ThreadSchlacht(self.__LEd2)
+        self.__Barraca3 = ThreadSchlacht(self.__LEd3)
+    
+        Grid.addWidget(self.__LEd1,0, 1, 1, 2)
+        Grid.addWidget(self.__LEd2,1, 1, 1, 2)
+        Grid.addWidget(self.__LEd3,2, 1, 1, 2)
+        Grid.addWidget(self.__Bt_Abertura,3, 0, 1, 2)
+        Grid.addWidget(self.__Bt_Fechamento,3, 2, 1, 2)
+    
+        self.setLayout(Grid)
+        self.show()
 
-        self._LEd1 = QLineEdit()
-        self._LEd2 = QLineEdit()
-        self._LEd3 = QLineEdit()
-
-        self._LEd1.setText('0')
-        self._LEd2.setText('0')
-        self._LEd3.setText('0')
-
-        self._Bt_Abertura = QPushButton('Abertura')
-        self._Bt_Fechamento = QPushButton('Fechamento')
-
-        self._Bt_Abertura.clicked.connect(self.action_abertura)
-        self._Bt_Fechamento.clicked.connect(self.action_fechamento)
-
-        # Adicionando os componentes ao Layout para que apareçam na tela
-        grid.addWidget(self._LEd1, 0, 0)
-        grid.addWidget(self._LEd2, 0, 1)
-        grid.addWidget(self._LEd3, 0, 2)
-        grid.addWidget(self._Bt_Abertura, 1, 0, 1, 3)
-        grid.addWidget(self._Bt_Fechamento, 2, 0, 1, 3)
-
-        self.Barraca1 = ThreadSchlacht(self._LEd1)
-        self.Barraca2 = ThreadSchlacht(self._LEd2)
-        self.Barraca3 = ThreadSchlacht(self._LEd3)
