ccff87bd08917c773facb6670e893bda11c9a2ce
spyestrela <ester130.10@gmail.com>
2026-06-08 19:30:51 -0300

    Enhance ThreadSchlacht with error handling and status check
    Added error handling for starting and stopping the thread, and implemented an isRunning method to check thread status.

--- Prog2/Vpls/VPL_10_Laboratorio_Virtual_de_Programação (PyQt5-Thread-Schlachtfest) (26_outubro_2023)/ThreadSchlacht.py
+++ Prog2/Vpls/VPL_10_Laboratorio_Virtual_de_Programação (PyQt5-Thread-Schlachtfest) (26_outubro_2023)/ThreadSchlacht.py
@@ -11,16 +11,33 @@ class ThreadSchlacht:
         self.__LEd = LEd_a
 
     def iniciar(self, Total_a):
-        self.__Total = Total_a
-        self.__Thr = threading.Thread(target=self.run)
-        self.__Thr.start()
+        try:
+            if self.__Thr is None:
+                self.__Total = Total_a
+                self.__Thr = threading.Thread(target=self.run)
+                self.__Thr.start()
+        except Exception as ex:
+            print("Error: unable to start thread: %s\n" % (ex))
+
     def parar(self):
-        self.__Total = 0;
-        self.__Thr = None;
+        try:
+            self.__Total = -1
+            self.__Thr = None
+        except Exception as ex:
+            print("Error: unable to stop thread: %s\n" % (ex))
+
+    def isRunning(self):
+        if self.__Thr is None:
+            return False
+        else:
+            return True
+
     def run(self):
-        while self.__Total >= 0:
-            self.__LEd.setText(str(int(self.__LEd.text()) + 1))
-            time.sleep(random.randint(1,7))
-            
+        while self.__Total > 0:
+            valor = int(self.__LEd.text()) + 1
+            self.__LEd.setText(str(valor))
+            tempo = random.random()
+            time.sleep(tempo)
             self.__Total += -1
+        self.__Thr = None
 
